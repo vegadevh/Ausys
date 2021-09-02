a56c45f44d46637e3178816d03052d2ec39d7dab
vegadevh <00148816@uca.edu.com>
2021-09-02 17:40:27 -0600

    Estado inicial de la aplicacion

---
+++ src/main/java/com/digitalatmosphere/ausys/AusysApplication.java
@@ -0,0 +1,13 @@
+package com.digitalatmosphere.ausys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AusysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AusysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalatmosphere/ausys/AusysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalatmosphere.ausys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AusysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
