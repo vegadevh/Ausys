eae202a0eb32e57cd0f213bd0e0efa77d6302cee
vegadevh <00148816@uca.edu.sv>
2021-10-26 15:24:12 -0600

    Conexion JPA post Merging

--- src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
+++ src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
@@ -40,7 +40,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/AUSYS");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("bf456edb00");
+		dataSource.setPassword("postgres");
 		return dataSource;
 	}
 	
