17e7b7072bdd9bf0659278208726149cd0f6532e
vegadevh <00148816@uca.edu.sv>
2021-11-05 18:34:44 -0600

    hotfix

--- src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
+++ src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
@@ -40,7 +40,7 @@ public class JPAConfiguration {
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/AUSYS");
 		dataSource.setUsername("postgres");
-		dataSource.setPassword("Alohomora");
+		dataSource.setPassword("saa");
 		return dataSource;
 	}
 	
