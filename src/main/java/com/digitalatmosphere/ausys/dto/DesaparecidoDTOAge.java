6fa57e3e903d6803ac909f6d27d9bbf55a0aa761
vegadevh <00148816@uca.edu.sv>
2021-11-08 16:39:24 -0600

    DTO para mapeo de edad en los registros

---
+++ src/main/java/com/digitalatmosphere/ausys/dto/DesaparecidoDTOAge.java
@@ -0,0 +1,58 @@
+package com.digitalatmosphere.ausys.dto;
+
+
+public class DesaparecidoDTOAge {
+	
+	private String id_desaparecido;
+	private String tipo_de_caso;
+	private String nombre;
+	private String apellido;
+	private String fecha_registro;
+	private String id_desaperi;
+	private String age;
+	
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getId_desaparecido() {
+		return id_desaparecido;
+	}
+	public void setId_desaparecido(String id_desaparecido) {
+		this.id_desaparecido = id_desaparecido;
+	}
+	public String getTipo_de_caso() {
+		return tipo_de_caso;
+	}
+	public void setTipo_de_caso(String tipo_de_caso) {
+		this.tipo_de_caso = tipo_de_caso;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getFecha_registro() {
+		return fecha_registro;
+	}
+	public void setFecha_registro(String fecha_registro) {
+		this.fecha_registro = fecha_registro;
+	}
+	public String getId_desaperi() {
+		return id_desaperi;
+	}
+	public void setId_desaperi(String id_desaperi) {
+		this.id_desaperi = id_desaperi;
+	}
+	
+	
+}
