77844e9f3463781ebd9d204d34c7fd8af6a43f6b
vegadevh <00148816@uca.edu.sv>
2021-11-11 09:56:02 -0600

    Configuracion de JPA

--- src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
+++ src/main/java/com/digitalatmosphere/ausys/JPAConfiguration.java
@@ -19,45 +19,49 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "com.digitalatmosphere.ausys")
 public class JPAConfiguration {
-	
+
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource());
 		em.setPersistenceUnitName("ausys");
 		em.setPackagesToScan("com.digitalatmosphere.ausys.domains");
-		
+
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
 		em.setJpaProperties(hibernateProperties());
-		
+
 		return em;
 	}
-	
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		// dataSource.setDriverClassName("org.postgresql.Driver");
+		// dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/AUSYS");
+		// dataSource.setUsername("postgres");
+		// dataSource.setPassword("postgres");
 		dataSource.setDriverClassName("org.postgresql.Driver");
 		dataSource.setUrl("jdbc:postgresql://ec2-44-199-40-188.compute-1.amazonaws.com:5432/df5gla92ke5s3o");
 		dataSource.setUsername("rqskvhxjnxbsup");
 		dataSource.setPassword("6b9ac6e3d02823de2cc2a50a98fd13c8838cb7954f169695a781221baca19219");
 		return dataSource;
 	}
-	
+
 	@Bean
 	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
 		JpaTransactionManager transactionManager = new JpaTransactionManager();
 		transactionManager.setEntityManagerFactory(entityManagerFactory);
 		return transactionManager;
 	}
-	
+
 	Properties hibernateProperties() {
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-		
+
 		properties.setProperty("hibernate.enable_lazy_load_no_trans", "true");
-		
+
 		return properties;
 	}
 
